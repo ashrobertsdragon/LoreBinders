34027ecbb43631b6be6ed0b9cc2836ba703167ef
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2023-12-17 17:20:35 -0700

    finished de-batching summarizer

--- characteranalysis.py
+++ characteranalysis.py
@@ -45,21 +45,21 @@ def get_attributes(folder_name: str) -> Tuple[str, str]:
     return role_attributes, custom_attributes
 
   ask_attributes = input("Besides characters and setting, what other attributes would you like ProsePal to search for (e.g. fantasy races, factions, religions, etc)? Please be as specific as possible. Attributes must be separated by commas for the program to work. Type N if you only want to search for characters and settings\n> ")
-  
+
   if ask_attributes.strip().lower() == "n":
     custom_attributes = ""
     role_attributes = ""
-    
+
   elif ask_attributes.strip() == "":
     custom_attributes = ""
     role_attributes = ""
-    
+
   else:
     custom_attributes = f" and the following additional attributes: {ask_attributes}"
   dictionary_attributes_list = ask_attributes.split(",")
 
   for attribute in dictionary_attributes_list:
-    
+
     attribute = attribute.strip()
     attribute_string = f"{attribute}:\n{attribute}1\n{attribute}2\n{attribute}3"
     attribute_strings.append(attribute_string)
@@ -108,7 +108,7 @@ def ner_role_script(folder_name) -> str:
 def search_names(chapters: list, folder_name: str, num_chapters: int, character_lists: list, character_lists_index: int) -> list:
 
   character_lists_path = f"{folder_name}/character_lists.json"
-  
+
   role_script = ner_role_script()
   model = "gpt_three"
   max_tokens = 1000
@@ -117,11 +117,11 @@ def search_names(chapters: list, folder_name: str, num_chapters: int, character_
   with tqdm(total = num_chapters, unit = "Chapter", ncols = 40, bar_format = "|{l_bar}{bar}|", position = 0, leave = True) as progress_bar:
     for chapter_index, chapter in enumerate(chapters):
       progress_bar.set_description(f"\033[92mProcessing chapter {chapter_index + 1} of {num_chapters}", refresh = True)
-      
+
       if chapter_index < character_lists_index:
         progress_bar.update(1)
         continue
-        
+
       chapter_number = chapter_index + 1
 
       prompt = f"Text: {chapter}"
@@ -138,7 +138,7 @@ def search_names(chapters: list, folder_name: str, num_chapters: int, character_
   return character_lists
 
 def character_analysis_role_script(attribute_table: dict, chapter_number: int) -> str:
-  
+
   instructions = (
     'You are a developmental editor helping create a story bible. '
     'For each character in the chapter, note their appearance, personality, '
@@ -172,7 +172,7 @@ def character_analysis_role_script(attribute_table: dict, chapter_number: int) -
     "Characters": character_schema,
     **other_attribute_schema
   })
-  
+
   role_script = f'{instructions}\n\n{attributes_json}'
 
 
@@ -195,7 +195,7 @@ def analyze_attributes(chapters: list, attribute_table: dict, folder_name: str, 
       chapter_number = i + 1
       progress_bar.set_description(f"\033[92mProcessing Chapter {i + 1}\033[0m", refresh = True)
       attribute_summary = ""
-      
+
       role_script = character_analysis_role_script(attribute_table, chapter_number)
       prompt = f"Chapter Text: {chapter}"
       model = "gpt-4-1106-preview"
@@ -204,7 +204,7 @@ def analyze_attributes(chapters: list, attribute_table: dict, folder_name: str, 
 
       chapter_summary[chapter_number] = attribute_summary
       cf.append_json_file(chapter_summary, chapter_summary_path)
-      
+
       progress_bar.update()
 
   cf.clear_screen()
@@ -218,47 +218,44 @@ def summarize_attributes(folder_name: str) -> None:
   """
 
   prompt_list = []
-  
+
   chapter_summaries_path = f"{folder_name}/chapter_summaries.json"
   chapter_summaries = cf.load_json_file(chapter_summaries_path)
 
   model_key = "gpt_three"
   temperature = 0.4
   max_tokens = 200
+  role_script = f"You are an expert summarizer. Please summarize the description over the course of the story for the following:"
 
-  for attribute, names in chapter_summaries.items():
-    for name, chapters in names.items():
+  for attribute, attribute_names in chapter_summaries.items():
+    for attribute_name, chapters in attribute_names.items():
       for chapter, details in chapters.items():
         if attribute == "Characters" or attribute == "Settings":
           description = ", ".join(f"{trait}: {','.join(detail)}" for trait, detail in details.items())
         else:
           description = ", ".join(details)
-
-        prompt_list.append(description)
-        role_script = f"You are an expert summarizer. For the following {attribute}, please summarize the description over the course of the story."
+        prompt_list.append(attribute, attribute_name, f"{attribute_name}: {description}")
 
   with tqdm(total = len(prompt_list), unit = "Prompt", ncols = 40) as progress_bar:
-    for i, prompt in enumerate(prompt_list):
+    for i, (attribute, attribute_name, prompt) in enumerate(prompt_list):
       progress_bar.set_description(f"\033[92mProcessing attriribute {i+1} of {len(prompt_list)}", refresh = True)      
       summary = cf.call_gpt_api(model_key, prompt, role_script, temperature, max_tokens)
-      
-      for (name_index, _), summary in zip(batch, batched_summaries):
-        name = batch[name_index[0]][1]
-        chapter_summaries[attribute][name]["summary"] = summary
-        cf.append_json_file(chapter_summaries, chapter_summaries_path)
+      chapter_summaries[attribute][attribute_name]["summary"] = summary
+      progress_bar.update(1)
+  cf.append_json_file(chapter_summaries, chapter_summaries_path)
 
 def analyze_book(user_folder: str, book_name: str):
   start_time = time.time()
-  
+
   # Prep work before doing the real work
   file_path = f"{user_folder}/{book_name}"
   sub_folder = os.path.basename(file_path).split('.')[0]
   folder_name = f"{user_folder}/{sub_folder}"
   os.makedirs(folder_name, exist_ok = True)
-  
+
   full_text = cf.read_text_file(file_path)
   chapters = cf.separate_into_chapters(full_text)
-  
+
   num_chapters, character_lists, character_lists_index, chapter_summary, chapter_summary_index = initialize_names(chapters, folder_name)
 
   # Named Entity Recognition  
@@ -277,7 +274,7 @@ def analyze_book(user_folder: str, book_name: str):
   else:
     print("Attribute table complete")
     attribute_table = cf.read_json_file(attribute_table_path)
-  
+
   # Semantic search based on attributes pulled
   if chapter_summary_index < num_chapters:
     print(f"Starting chapter summaries at chapter {chapter_summary_index + 1}")
