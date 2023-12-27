c3abd9ce6b22c96a3f5993d8a31b06ebfd827739
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2023-12-26 17:25:11 -0700

    removed dead code from main

--- main.py
+++ main.py
@@ -14,9 +14,8 @@ def main():
   user_email = os.getenv("user_email")
   user_folder = os.path.join("ProsePal", "users", user)
 
-  #book_name = input("Enter the file name of the book (including the .txt extension): ")
-  book_name = "DragonRun.txt"
-  narrator = "Kalia"
+  book_name = "DragonRun.txt" # placeholder
+  narrator = "Kalia" # placeholder
 
   folder_name = characteranalysis.analyze_book(user_folder, book_name, narrator)
   create_pdf(folder_name, book_name)
