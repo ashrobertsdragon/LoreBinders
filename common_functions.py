805fa8704060e9e9f7c8494be96fb5c2e3996bff
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-01-01 11:17:16 -0700

    better error handling to catch unresolvable exceptions, and created kill_app function to DRY code for unresolvable errors

--- common_functions.py
+++ common_functions.py
@@ -3,7 +3,9 @@ import json
 import os
 import re
 import time
+from typing import Any
 
+import openai
 import tiktoken
 from openai import OpenAI
 
@@ -15,10 +17,15 @@ logging.basicConfig(filename='api_calls.log', level=logging.INFO,
 if not os.path.exists(".replit"):
   from dotenv import load_dotenv
   load_dotenv()
+
+def kill_app(e):
+  logging.critical(e)
+  email_error(e)
+  exit(1)
+
 api_key = os.environ.get("OPENAI_API_KEY")
 if not api_key:
-  logging.error("OPENAI_API_KEY environment variable not set")
-  raise Exception("OPENAI_API_KEY environment variable not set")
+  kill_app("OPENAI_API_KEY environment variable not set")
 
 OPENAI_CLIENT = OpenAI()
 
@@ -80,6 +87,7 @@ def check_json(json_str: str, attempt_count: int = 0) -> str:
 
 
 def append_to_dict_list(dictionary, key, value):
+  "Appends value to list of values in dictionary"
   if key in dictionary:
       dictionary[key].append(value)
   else:
@@ -93,45 +101,40 @@ def clear_screen():
     os.system('clear')
 
 def read_text_file(file_path: str):
+  "Opens and reads text file"
   try:
     with open(file_path, "r") as f:
       read_file = f.read()
     return read_file
   except FileNotFoundError:
-    logging.error(f"Error: File '{file_path}' not found.")
-    email_error(f"Error: File '{file_path}' not found")
-    exit()
+    kill_app(f"Error: File '{file_path}' not found.")
+
 
 def read_json_file(file_path: str):
+  "Opens and reads JSON file"
   try:
     with open(file_path, "r") as f:
       read_file = json.load(f)
     return read_file
-  except FileNotFoundError:
-    logging.error(f"Error: File '{file_path}' not found.")
-    email_error(f"Error: File '{file_path}' not found.")
-    exit()
-  except json.decoder.JSONDecodeError:
-    logging.error(f"Error: File '{file_path}' not valid JSON.")
-    email_error(f"Error: File '{file_path}' not vaid JSON")
-    exit()
   except Exception as e:
-    logging.error(f"Error: {e}")
-    email_error(e)
-    exit()
+    kill_app(e)
 
 def write_to_file(content, file_path):
+  "Appends content to text file on new line"
   with open(file_path, "a") as f:
     f.write(content + "\n")
 
 def separate_into_chapters(text: str) -> list:
+  "Splits string at delimeter of three asterisks"
   return re.split("\s*\*\*\s*", text)
 
 def write_json_file(content, file_path: str):
+  "Writes JSON file"
   with open(file_path, "w") as f:
     json.dump(content, f, indent=2)
 
 def append_json_file(content, file_path: str):
+  "Reads JSON file, and adds content to datatype before overwriting"
   if os.path.exists(file_path):
     read_file = read_json_file(file_path)
   else:
@@ -143,13 +146,14 @@ def append_json_file(content, file_path: str):
   write_json_file(read_file, file_path)
 
 def check_continue():
+  "Asks user to check output before continuing"
   continue_program = ""
   while continue_program.upper() not in ["Y", "N"]:
     continue_program = input("If this looks right, type Y to continue the program. Type N to exit: ")
     if continue_program.upper() == "N":
       print("Exiting the program...")
       logging.info("User exited the program...")
-      exit()
+      exit(0)
     elif continue_program.upper() != "Y":
       print("Invalid input. Please try again.")
       logging.info("Invalid input. Please try again.")
@@ -165,12 +169,12 @@ def get_model_details(model_key: str) -> dict:
     "gpt_three": {
       "model_name": "gpt-3.5-turbo-1106",
       "rate_limit": 90000,
-        "context_window": 16000
+      "context_window": 16000
     },
     "gpt_four": {
       "model_name": "gpt-4-1106-preview",
       "rate_limit": 300000,
-        "context_window": 128000
+      "context_window": 128000
     }
   }
   return model_details.get(model_key, {"model_name": None, "rate_limit": 250000, "context_window": 4096})
@@ -183,15 +187,47 @@ def is_rate_limit(model_key: str) -> int:
   return model_details["rate_limit"]
 
 def count_tokens(text):
+  "Counts tokens using OpenAI's tiktoken tokenizer"
   tokenizer = tiktoken.get_encoding("cl100k_base")
   return len(tokenizer.encode(text))
 
-def error_handle(e, retry_count):
+def error_handle(e: Any, retry_count: int) -> int:
+  """
+  Determines whether error is unresolvable or should be retried. If unresolvable,
+  error is logged and administrator is emailed before exit. Otherwise, exponential
+  backoff is used for up to 5 retries.
+
+  Args:
+    e: an Exception body
+    retry_count: the number of attempts so far
+
+  Returns:
+    retry_count: the number of attemps so far
+  """
+
+  unresolvable_errors = [
+    openai.BadRequestError,
+    openai.AuthenticationError,
+    openai.NotFoundError,
+    openai.PermissionDeniedError,
+    openai.UnprocessableEntityError
+  ]
+
+  error_code = getattr(e, "status_code", None)
+  error_details = getattr(e, "response", {}).json().get("error", {})
+  error_message = error_details.get("message", "Unknown error")
+
+  if isinstance(e, tuple(unresolvable_errors)):
+    kill_app(e)
+  if error_code == 401:
+    kill_app(e)
+  if "exceeded your current quota" in error_message:
+    kill_app(e)
+
+  logging.exception(e)
   retry_count += 1
   if retry_count == 5:
-    logging.error("Maximum retry count reached")
-    email_error("retry count reached")
-    exit()
+    kill_app("Maximum retry count reached")
   else:
     sleep_time = (5 - retry_count)  + (retry_count ** 2)
     logging.warning(f"Retry attempt #{retry_count} in {sleep_time} seconds.")
@@ -199,6 +235,23 @@ def error_handle(e, retry_count):
   return retry_count
 
 def call_gpt_api(model_key, prompt, role_script, temperature, max_tokens, response_type = None, retry_count = 0, assistant_message = None):
+  """
+  Makes API calls to the OpenAI ChatCompletions engine.
+
+  Args:
+    model_key (str): The key of the GPT model to use.
+    prompt (str): The user's prompt.
+    role_script (str): The system's role script.
+    temperature (float): The randomness of the generated text.
+    max_tokens (int): The maximum number of tokens in the generated text.
+    response_type (str, optional): The desired response format ("json" or "text"). Defaults to None.
+    retry_count (int, optional): The number of retry attempts. Defaults to 0.
+    assistant_message (str, optional): The assistant's message. Defaults to None.
+
+  Returns:
+    str: The generated content from the OpenAI GPT-3 model.
+  """
+
   rate_limit_data = read_json_file("rate_limit.json") if os.path.exists("rate_limit.json") else {}
   rate_limit_data["tokens_used"] = rate_limit_data.get("tokens_used", 0)
   rate_limit_data["minute"] = rate_limit_data.get("minute", time.time())
@@ -260,14 +313,13 @@ def call_gpt_api(model_key, prompt, role_script, temperature, max_tokens, respon
     if response.choices and response.choices[0].message.content:
       content = response.choices[0].message.content.strip()
       tokens = response.usage.total_tokens
-      rate_limit_data["tokens_used"]  += tokens
+      rate_limit_data["tokens_used"] += tokens
       write_json_file(rate_limit_data, "rate_limit_data.json")
     else:
       logging.error("No message content found")
       raise Exception("No message content found")
 
   except Exception as e:
-    logging.exception(e)
     retry_count = error_handle(e, retry_count)
     call_gpt_api(model_key, prompt, role_script, temperature, max_tokens, response_type, retry_count, assistant_message)
 
@@ -275,6 +327,7 @@ def call_gpt_api(model_key, prompt, role_script, temperature, max_tokens, respon
     answer =  assistant_message.rstrip("}") + content.lstrip("{")
     print(answer)
     check_continue()
+    answer = content
   else:
     answer = content
 
