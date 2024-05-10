ee6bc6f1063a7498a24f725e587eb3715cfe6e67
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-05-09 20:44:49 -0600

    book class complete

--- lb_new/book.py
+++ lb_new/book.py
@@ -1,4 +1,5 @@
 from file_handling import FileHandler
+from error_handler import ErrorHandler
 
 files = FileHandler()
 
@@ -7,18 +8,28 @@ class Book():
     A book or collection of chapters
     """
 
-    def __init__(self, file_path):
+    def __init__(self, book_dict):
         """
         Initializes the Book object by reading the input file and splitting it
         into chapters.
         """
-        self.book = files.read_text_file(file_path)
-    
+        self.book_dict = book_dict
+        self.__dict__ = {key: book_dict[key] for key in book_dict.keys()}
+        self.file = files.read_text_file(self.file_path)
+        self.error_handler = ErrorHandler(__name__)
+
+    def __name__(self):
+        self.name = self.title
+
     def get_chapters(self):
         """
         Returns a list of Chapter objects
         """
-        self.chapters = [Chapter(number, text) for number, text in enumerate(files.split_into_chapters(self.book), start=1)]
+        chapters = []
+        for number, text in enumerate(files.split_into_chapters(self.file), start=1):
+            chapters.append(Chapter(number, text))
+        self.chapters = chapters
+        return chapters
 
 class Chapter():
     """
