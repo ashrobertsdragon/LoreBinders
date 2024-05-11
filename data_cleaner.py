0c745d998be4ea91bb1ad567ce91381358383a86
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-05-11 11:12:31 -0600

    moved to_singular into class

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,3 +1,5 @@
+import re
+
 class DataCleaner:
     """
     Responsible for cleaning the extracted and analyzed data.
@@ -25,3 +27,35 @@ class DataCleaner:
         Takes the cleaned nested dictionary from DataCleaner and reshapes the data structure.
         Returns the reshaped nested dictionary.
         """
+
+    def to_singular(self, plural: str) -> str:
+        """
+        Converts a plural word to its singular form based on common English
+        pluralization rules.
+            
+        Args:
+            plural: A string representing the plural form of a word.
+            
+        Returns:
+            (str) The singular form of the given word if a pattern matches,
+                otherwise the original word.
+        """
+        patterns = {
+            r'(\w+)(ves)$': r'\1f',
+            r'(\w+)(ies)$': r'\1y',
+            r'(\w+)(i)$': r'\1us',
+            r'(\w+)(a)$': r'\1um',
+            r'(\w+)(en)$': r'\1an',
+            r'(\w+)(oes)$': r'\1o',
+            r'(\w+)(ses)$': r'\1s',
+            r'(\w+)(hes)$': r'\1h',
+            r'(\w+)(xes)$': r'\1x',
+            r'(\w+)(zes)$': r'\1z'
+        }
+
+        for pattern, repl in patterns.items():
+            singular = re.sub(pattern, repl, plural)
+            if plural != singular:
+                return singular
+        return plural[:-1]
+    
