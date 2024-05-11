144faa6908cbe4367ed91751c4fbd34ae8a920d0
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-05-11 00:20:05 -0600

    _types now includes Book and Chapter classes

--- _types.py
+++ _types.py
@@ -2,6 +2,8 @@ from typing import Literal
 
 from openai.types.chat.chat_completion import ChatCompletion
 
+from book import Book, Chapter
+
 FinishReason = Literal["stop", "length", "tool_calls", "content_filter", "function_call"]
 
-__all__ = [FinishReason, ChatCompletion]
+__all__ = [FinishReason, ChatCompletion, Book, Chapter]
