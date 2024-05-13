2abba6e530678c6dcb2c7565e86e10abeb5206df
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-05-12 19:09:52 -0600

    added add_analysis method to Chapter class and cleaned up spacing

--- book.py
+++ book.py
@@ -16,11 +16,12 @@ class Book():
         self.book_dict = book_dict
         for key, value in book_dict.items():
             setattr(self, key, value)
-        self.file_handler = FileHandler()
-        self.file = self.file_handler.read_text_file
-        self.chapters = self.get_chapters()
-        (self.file_path)
+
         self.error_handler = ErrorHandler(__name__)
+        self.file_handler = FileHandler()
+
+        self.file = self.file_handler.read_text_file(self.file_path)
+        self.chapters = self.get_chapters()
 
     def __name__(self):
         self.name = self.title
@@ -29,7 +30,7 @@ class Book():
         """
         Returns a list of Chapter objects
         """
-        chapters = []
+        chapters: list = []
         for number, text in enumerate(self.file_handler.split_into_chapters(self.file), start=1):
             chapters.append(Chapter(number, text))
         self.chapters = chapters
@@ -46,5 +47,8 @@ class Chapter():
         self.number: int = number
         self.text: str = text
 
-    def add_attributes(self, attributes: list) -> None:
-        self.attributes = attributes
+    def add_names(self, names: list) -> None:
+        self.names: list = names
+    
+    def add_analysis(self, analysis: dict) -> None:
+        self.summary: dict = analysis
