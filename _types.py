b2e904f33eb12ccfb17d0f310320978a5c52a5f8
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-05-14 22:05:50 -0600

    added Logger to _types

--- _types.py
+++ _types.py
@@ -1,3 +1,4 @@
+from logging import Logger
 from typing import Literal
 
 from openai.types.chat.chat_completion import ChatCompletion
@@ -6,4 +7,4 @@ from book import Book, Chapter
 
 FinishReason = Literal["stop", "length", "tool_calls", "content_filter", "function_call"]
 
-__all__ = [FinishReason, ChatCompletion, Book, Chapter]
+__all__ = [FinishReason, ChatCompletion, Book, Chapter, Logger]
