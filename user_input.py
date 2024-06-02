2980b35bafe5b9a634e7ddedbf293bb0c76af0f6
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-06-02 12:34:48 -0600

    finished get_attributes function

--- user_input.py
+++ user_input.py
@@ -1,5 +1,6 @@
 import os
-from typing import Optional
+from typing import List, Optional
+
 
 def get_narrator() -> str:
     """
@@ -12,16 +13,21 @@ def get_narrator() -> str:
     """
     valid_input: bool = False
     while not valid_input:
-        third_p_input: str = input("Is the book written in third person? Y/n > ")
+        third_p_input: str = input(
+            "Is the book written in third person? Y/n > "
+        )
         if third_p_input.upper().startswith("Y"):
             is_third: bool = True
         elif third_p_input.lower().startswith("n"):
-            is_third: bool = False
-    
+            is_third = False
+
     if is_third:
         return ""
     else:
-        return input("Enter narrator's name as identified by most characters in the story > ")
+        return input(
+            "Enter narrator's name as identified by most of the "
+            "characters in the story > "
+        )
 
 
 def get_attributes(attribute_type: Optional[str] = None) -> list:
@@ -36,20 +42,40 @@ def get_attributes(attribute_type: Optional[str] = None) -> list:
     Returns:
         A list of user inputted attributes.
     """
+    if attribute_type is None:
+        input_label = "category"
+    else:
+        input_label = f"{attribute_type} trait"
+    attribute_list: List[str] = []
+    while True:
+        attribute = input(
+            f"Enter additional {input_label} or press D for done > "
+        )
+        if attribute.upper().strip() == "D":
+            break
+        attribute_list.append(attribute)
+    return attribute_list
+
 
 def parse_file_path(path) -> str:
     path_parts: str = path.split("/") if "/" in path else path.split("\\")
     return os.path.join(path_parts)
 
+
 def input_title() -> str:
     return input("Enter book title > ")
+
 
 def input_author() -> str:
     return input("Enter author name > ")
 
+
 def input_book_path() -> str:
-    file_path: str = input("Enter the absolute path to the ebook to process > ")
+    file_path: str = input(
+        "Enter the absolute path to the ebook to process > "
+    )
     return parse_file_path(file_path)
+
 
 def get_inputs() -> dict:
     title: str = input_title()
@@ -65,12 +91,14 @@ def get_inputs() -> dict:
         "book_file": book_path,
         "narrator": narrator,
         "character_attributes_list": character_attributes,
-        "other_categories_list": other_categories
+        "other_categories_list": other_categories,
     }
-    
+
+
 def get_book() -> dict:
     book_dict: dict = get_inputs()
     return confirm_inputs(book_dict)
+
 
 def confirm_inputs(book_dict: dict) -> dict:
     """
@@ -107,7 +135,7 @@ def confirm_inputs(book_dict: dict) -> dict:
             "narrator": get_narrator,
             "book_file": input_book_path,
             "character_attributes": get_attributes("characters"),
-            "other_attributes": get_attributes()
+            "other_attributes": get_attributes(),
         }
         new_value = input_function[key_name]()
         book_dict[key_name] = new_value
@@ -118,7 +146,10 @@ def confirm_inputs(book_dict: dict) -> dict:
             key_name = key_name_map[key]
             print(f"  - {key_name}: {value}")
 
-        choice = input("\nIf this is correct, type Y to continue, otherwise, enter the item you wish to change > ").lower()
+        choice = input(
+            "\nIf this is correct, type Y to continue. "
+            "Otherwise, enter the item you wish to change > "
+        ).lower()
 
         if choice.upper() == "Y":
             return book_dict
@@ -129,11 +160,15 @@ def confirm_inputs(book_dict: dict) -> dict:
                 key_name = list(book_dict.keys())[index]
                 edit_book_dict(key_name)
             else:
-                print("Invalid index. Please enter a valid number or key name.")
+                print(
+                    "Invalid index." "Please enter a valid number or key name."
+                )
         else:
             key_name = choice.strip()
             if key_name in key_name_map.keys():
                 edit_book_dict(key_name)
             else:
-                print("Invalid key name. Please enter a valid number or key name.")
-
+                print(
+                    "Invalid key name."
+                    "Please enter a valid number or key name."
+                )
