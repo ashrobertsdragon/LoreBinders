56a198143a678931ca42c53b6b52608370b21186
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-06-20 16:35:11 -0600

    Refactor main module to move most functions to build_lorebinder to allow for multiple input sources. main.main() now only calls user_input.get_book() and then build_lorebinder.run()

--- src/lorebinders/main.py
+++ src/lorebinders/main.py
@@ -1,80 +1,12 @@
-import os
-
-from binders import Binder
-from book import Book
-from book_dict import BookDict
 from dotenv import load_dotenv
-from ebook2text.convert_file import convert_file  # type: ignore
-from make_pdf import create_pdf
+
+from build_lorebinder import run
 from user_input import get_book
-
-
-def create_book(book_dict: BookDict) -> Book:
-    return Book(book_dict)
-
-
-def create_lorebinder(book: Book, ai_model) -> Binder:
-    return Binder(book, ai_model)
-
-
-def build_binder(lorebinder: Binder) -> None:
-    lorebinder.build_binder()
-
-
-def create_folder(user: str) -> str:
-    user_folder = os.path.join("work", user)
-    os.makedirs(user_folder, exist_ok=True)
-    return user_folder
-
-
-def create_user(author: str) -> str:
-    names = author.split(" ")
-    return "_".join(names)
-
-
-def create_user_folder(author: str) -> str:
-    user = create_user(author)
-    return create_folder(user)
-
-
-def create_txt_filename(book_dict: BookDict, book_file: str) -> None:
-    base, _ = os.path.splitext(book_file)
-    txt_filename = f"{base}.txt"
-    book_dict.txt_file = txt_filename
-
-
-def convert(file_path: str, limited_metadata: dict) -> None:
-    convert_file(file_path, limited_metadata)
-
-
-def create_limited_metadata(book_dict: BookDict) -> dict:
-    author = book_dict.author
-    title = book_dict.title
-    return {"title": title, "author": author}
-
-
-def convert_book_file(book_dict: BookDict) -> None:
-    book_file = book_dict.book_file
-    author = book_dict.author
-
-    user_folder = create_user_folder(author)
-    file_path = os.path.join(user_folder, book_file)
-    limited_metadata = create_limited_metadata(book_dict)
-    convert(file_path, limited_metadata)
-    create_txt_filename(book_dict, book_file)
 
 
 def main():
     book_dict = get_book()
-
-    convert_book_file(book_dict)
-
-    book = create_book(book_dict)
-    ai_model = os.getenv("ai_model")  # Placeholder
-    lorebinder = create_lorebinder(book, ai_model)
-    build_binder(lorebinder)
-    if book_dict.user_folder is not None:
-        create_pdf(book_dict.user_folder, book_dict.title)
+    run(book_dict)
 
 
 if __name__ == "__main__":
