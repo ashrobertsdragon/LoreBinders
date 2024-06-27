0b00754954a048a0af498a80e6887c11f43c3ef1
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-06-27 17:26:30 -0600

    Add 'lorebinders' to path in conftest

--- conftest.py
+++ conftest.py
@@ -3,3 +3,6 @@ import sys
 
 src_path = pathlib.Path(__file__).parent / "src"
 sys.path.insert(0, str(src_path))
+
+lorebinders_path = src_path / "lorebinders"
+sys.path.insert(0, str(lorebinders_path))
