1b7916092b3976833a6415ff7c8c842761ba6347
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-07-06 12:48:19 -0600

    Add logger capture

--- conftest.py
+++ conftest.py
@@ -1,8 +1,19 @@
 import pathlib
 import sys
 
+from loguru import logger
+
 src_path = pathlib.Path(__file__).parent / "src"
 sys.path.insert(0, str(src_path))
 
 lorebinders_path = src_path / "lorebinders"
 sys.path.insert(0, str(lorebinders_path))
+
+
+def pytest_configure(config):
+    logger.remove()  # Remove default handler
+    logger.add(sys.stderr, level="DEBUG")
+
+
+def pytest_unconfigure(config):
+    logger.remove()  # Clean up handlers
