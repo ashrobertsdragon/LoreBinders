525100fb8ddb176289a2d563807d62c8a4b72b53
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-08-10 07:01:48 -0600

    Fix find_full_object to return position if any complete JSON objects found

--- src/lorebinders/json_tools.py
+++ src/lorebinders/json_tools.py
@@ -49,124 +49,147 @@ def json_str_to_dict(json_str: str) -> dict:
     return repair_json(json_str)
 
 
-class MergeJSON:
+# Merge JSON objects
+def build_repair_stub(
+    first_part: str, second_part: str, first_end: int, second_start: int
+) -> str:
     """
-    A class to merge two partial JSON objects and validate the combined
-    JSON string.
+    Builds a string containing details of the merge operation.
 
-    Attributes:
-        first_half (str): The first segment of a partial JSON object in string
-            form.
-        second_half (str): The second segment of a partial JSON object in
-            string form.
-        repair_stub (str): A formatted string containing the first and second
-            response halves.
-        first_end (int): The index of the end of the first JSON object in the
-            first segment.
-        second_start (int): The index of the start of the second JSON object
-            in the second segment.
+    Args:
+        first_part (str): The first potential partial JSON object.
+        second_part (str): The second potential partial JSON object.
+        first_end (int): The end index of the first JSON object in the first
+            segment, if any.
+        second_start (int): The start index of the second JSON object in the
+            second segment, if any.
 
-    Methods:
-        merge:
-            Merges the two partial JSON object segments and returns the
-                combined string.
+    Returns:
+        str: A string containing details of the merge operation.
+    """
+    repair_stub = []
+    for index, part in zip(
+        (first_end, second_start), (first_part, second_part)
+    ):
+        if index == 0:
+            stub = f"{part}\nhas no complete object"
+        else:
+            stub = f"{part}\ncomplete object at index {index}"
+        repair_stub.append(stub)
+    return "\n".join(repair_stub)
 
-        is_valid:
-            Validates the combined JSON string by attempting to load it as
-                JSON.
 
-    Warnings:
-        If the merge operation fails, a warning is logged with details of the
-            failure.
-        If the validation fails, an error is logged with details of the
-            failure.
+def log_merge_warning(
+    first_part: str, second_part: str, first_end: int, second_start: int
+) -> None:
+    """
+    Logs a warning with details of the merge operation.
+
+    Args:
+        first_part (str): The first partial JSON object.
+        second_part (str): The second partial JSON object.
+
+    Returns:
+        None
     """
 
-    def _set_halves(self, first_half: str, second_half: str) -> None:
-        """
-        Sets the end indices of the first and second JSON objects in the
-        first and second segments, respectively.
-        """
-        self.first_half = first_half
-        self.second_half = second_half
+    repair_stub = build_repair_stub(
+        first_part, second_part, first_end, second_start
+    )
+    log = f"Could not combine.\n{repair_stub}"
+    logger.warning(log)
 
-        self.repair_stub = (
-            f"First response:\n{first_half}\nSecond response:\n{second_half}"
-        )
 
-    def find_ends(self) -> tuple[int, int]:
-        """
-        Finds the end index of the first JSON object in the first segment and
-        the start index of the second JSON object in the second segment.
-        """
-        first_end = self._find_full_object(self.first_half, forward=False)
-        second_start = self._find_full_object(self.second_half)
-        return first_end, second_start
+def find_ends(first_part: str, second_part: str) -> tuple[int, int]:
+    """
+    Finds the end index of the first JSON object in the first segment and
+    the start index of the second JSON object in the second segment.
 
-    @staticmethod
-    def _find_full_object(string: str, forward: bool = True) -> int:
-        """
-        Finds the position of the first full object of a string representation
-        of a partial JSON object.
-        """
+    Args:
+        first_part (str): The first partial JSON object.
+        second_part (str): The second partial JSON object.
 
-        balanced: int = 0 if forward else -1
-        count: int = 0
-        last_balanced_position: int = 0
+    Returns:
+        tuple[int, int]: The end index of the first JSON object in the first
+            segment and the start index of the second JSON object in the
+            second segment.
+    """
+    first_end = find_full_object(first_part, forward=False)
+    second_start = find_full_object(second_part)
+    return first_end, second_start
 
-        directional_string: str = string if forward else string[::-1]
-        for i, char in enumerate(directional_string):
-            if char == "{":
-                count += 1
-            elif char == "}":
-                count -= 1
-            if count == balanced:
-                last_balanced_position = i
 
-        if count != 0:
-            return 0
+def find_full_object(string: str, forward: bool = True) -> int:
+    """
+    Finds the position of the first full object of a string representation
+    of a partial JSON object.
 
-        return (
-            last_balanced_position if forward else last_balanced_position + 1
-        )
+    Iterates from the 'broken' end of a partial JSON object and returns the
+    position of the end of the first complete object in the string from the
+    direction specified. If no complete object is found, returns 0.
+    If traversing the string in the reverse direction (forward=False), the
+    position is still the RIGHT end of the first complete object.
 
-    def _merge_halves(self, first_end: int, second_start: int) -> str:
-        """
-        Merges two strings of a partial JSON object
+    Args:
+        string (str): The string representation of a partial JSON object.
+        forward (bool, optional): Flag to trigger the traversing direction.
+            True is left to right. False is right to left. Defaults to True.
 
-        Args:
-            first_half: str - the first segment of a partial JSON object in
-                string form
-            second_half: str - the second segment of a partial JSON object in
-                string form
+    Returns:
+        int: The position of the end of the first complete object in the
+            string from the direction specified.
+    """
 
-        Returns either the combined string of a full JSON object or empty
-            string.
-        """
+    balanced: int = 0 if forward else -1
+    count: int = 0
+    directional_string: str = string if forward else string[::-1]
 
-        if first_end and second_start:
-            return (
-                self.first_half[:first_end]
-                + ", "
-                + self.second_half[second_start:]
-            )
-        log = f"Could not combine.\n{self.repair_stub}"
-        logger.warning(log)
-        return ""
+    for i, char in enumerate(directional_string):
+        if char == "{":
+            count += 1
+        elif char == "}":
+            count -= 1
+        if count == balanced:
+            # return the position from the end being iterated from
+            return i if forward else i + 1
 
-    def merge(self, first_half: str, second_half: str) -> str:
-        """
-        Merges two strings of a partial JSON object
+    return 0
 
-        Args:
-            first_half: str - the first segment of a partial JSON object in
-                string form
-            second_half: str - the second segment of a partial JSON object in
-                string form
 
-        Returns either the combined string of a full JSON object or empty
-            string.
-        """
-        self._set_halves(first_half, second_half)
-        first_end, second_start = self.find_ends()
-        return self._merge_halves(first_end, second_start)
+def merge_halves(
+    first_part: str, second_part: str, first_end: int, second_start: int
+) -> str:
+    """
+    Merges two strings of a partial JSON object
+
+    Args:
+        first_part: str - the first segment of a partial JSON object in
+            string form
+        second_part: str - the second segment of a partial JSON object in
+            string form
+
+    Returns either the combined string of a full JSON object or empty
+        string.
+    """
+    if first_end and second_start:
+        return f"{first_part[:first_end]},{second_part[second_start:]}"
+    log_merge_warning(first_part, second_part, first_end, second_start)
+    return ""
+
+
+def merge_json(first_part: str, second_part: str) -> str:
+    """
+    Merges two strings of a partial JSON object
+
+    Args:
+        first_part: str - the first segment of a partial JSON object in
+            string form
+        second_part: str - the second segment of a partial JSON object in
+            string form
+
+    Returns either the combined string of a full JSON object or empty
+        string.
+    """
+
+    first_end, second_start = find_ends(first_part, second_part)
+    return merge_halves(first_part, second_part, first_end, second_start)
