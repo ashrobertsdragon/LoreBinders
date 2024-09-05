eab8088d4c50d1c28dfa6b0d7e2eec5a1f863184
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-09-05 16:57:15 -0600

    Refactor create_pdf to use helper functions and constants

--- src/lorebinders/make_pdf.py
+++ src/lorebinders/make_pdf.py
@@ -13,7 +13,6 @@ from reportlab.lib.styles import (
 )
 from reportlab.platypus import (
     Image,
-    ListFlowable,
     ListItem,
     PageBreak,
     Paragraph,
@@ -23,12 +22,19 @@ from reportlab.platypus import (
 from reportlab.platypus.tableofcontents import TableOfContents
 
 if TYPE_CHECKING:
-    from lorebinders._type_annotations import Book, BookDict
+    from lorebinders._type_annotations import Book, BookDict, Flowable
 
 
 PAGE_SIZE = LETTER
-IMAGE_WIDTH = 200
-IMAGE_HEIGHT = 200
+IMAGE_WIDTH = IMAGE_HEIGHT = 200
+
+STYLE_NORMAL = "Normal"
+STYLE_TITLE = "Title"
+STYLE_HEADING1 = "Heading1"
+STYLE_HEADING2 = "Heading2"
+STYLE_HEADING3 = "Heading3"
+
+NamesDict = dict[str, dict[str, list[str] | str] | list[str] | str]
 
 
 class AfterSection(Enum):
@@ -56,7 +62,7 @@ def create_detail_list(
                 "\n".join(details)
                 if isinstance(details, list)
                 else details.replace(", ", "\n"),
-                style["Normal"],
+                style[STYLE_NORMAL],
             ),
             bulletType="1",
             value=int(chapter),
@@ -77,25 +83,18 @@ def setup_toc(style: StyleSheet1) -> TableOfContents:
         TableOfContents: The table of contents.
     """
     toc = TableOfContents()
-    toc_style_attributes = ParagraphStyle(
-        "TOCLevel0",
-        parent=style["Normal"],
-        fontSize=12,
-        leading=14,
-        spaceAfter=6,
-        spaceBefore=6,
-    )
-
-    toc_style_names = ParagraphStyle(
-        "TOCLevel1",
-        parent=style["Normal"],
-        fontSize=10,
-        leading=12,
-        spaceAfter=3,
-        spaceBefore=3,
-        leftIndent=10,
-    )
-    toc.levelStyles = [toc_style_attributes, toc_style_names]
+    toc.levelStyles = [
+        ParagraphStyle(
+            f"TOCLevel{i}",
+            parent=style[STYLE_NORMAL],
+            fontSize=12 - 2 * i,
+            leading=14 - 2 * i,
+            spaceAfter=6 - 3 * i,
+            spaceBefore=6 - 3 * i,
+            leftIndent=10 * i,
+        )
+        for i in range(2)
+    ]
     return toc
 
 
@@ -114,7 +113,7 @@ def create_paragraph(text: str, style: StyleSheet1) -> Paragraph:
 
 
 def add_item_to_story(
-    story: list, after_section: AfterSection, *flowables
+    story: list[Flowable], after_section: AfterSection, *flowables
 ) -> None:
     """
     Adds one or more flowables to the story.
@@ -126,6 +125,51 @@ def add_item_to_story(
     """
     story.extend(iter(flowables))
     story.append(after_section)
+
+
+def add_image(story: list[Flowable], image_path: str) -> None:
+    """
+    Adds an image to the story.
+
+    Args:
+        story (list): The story to add the image to.
+        image_path (str): The path to the image.
+    """
+    if os.path.exists(image_path):
+        img = Image(image_path, width=IMAGE_WIDTH, height=IMAGE_HEIGHT)
+        add_item_to_story(story, AfterSection.SPACER, img)
+
+
+def add_content(
+    story: list[Flowable],
+    content: NamesDict,
+    style: StyleSheet1,
+    is_traits: bool = False,
+) -> None:
+    """
+    Adds content to the story.
+
+    Args:
+        story (list): The story to add the content to.
+        content (dict[str, dict[str, list[str] | str] | list[str] | str]): The
+            content to add.
+        style (StyleSheet1): The style to use for the content.
+        is_traits (bool, optional): Whether the content is for the traits
+            section. Defaults to False.
+    """
+    for key, value in content.items():
+        if key == "summary":
+            continue
+        if is_traits:
+            trait = Paragraph(key, style[STYLE_HEADING3])
+        detail_list = create_detail_list(
+            cast(dict, value) if is_traits else content, style
+        )
+        add_item_to_story(
+            story,
+            AfterSection.PAGE_BREAK,
+            (trait, detail_list) if is_traits else detail_list,
+        )
 
 
 def initialize_pdf(metadata: BookDict) -> tuple[SimpleDocTemplate, str]:
@@ -141,8 +185,8 @@ def initialize_pdf(metadata: BookDict) -> tuple[SimpleDocTemplate, str]:
     """
     title = metadata.title
     author = metadata.author
-    if folder_name := metadata.user_folder:
-        output_path = Path(folder_name, f"{title}-lorebinder.pdf")
+    folder_name = cast(str, metadata.user_folder)
+    output_path = Path(folder_name, f"{title}-lorebinder.pdf")
     doc = SimpleDocTemplate(
         output_path, pagesize=PAGE_SIZE, author=author, title=title
     )
@@ -156,13 +200,15 @@ def create_pdf(book: Book) -> None:
     Args:
         book (Book): The book object.
     """
-    binder: dict[str, dict[str, dict[str, dict | str]]] = book.binder
+    binder: dict[str, dict[str, NamesDict]] = book.binder
 
-    story: list = []
+    story: list[Flowable] = []
     styles: StyleSheet1 = getSampleStyleSheet()
     doc, title = initialize_pdf(book.metadata)
 
-    title_page = create_paragraph(f"LoreBinder\nfor\n{title}", styles["Title"])
+    title_page = create_paragraph(
+        f"LoreBinder\nfor\n{title}", styles[STYLE_TITLE]
+    )
     add_item_to_story(story, AfterSection.PAGE_BREAK, title_page)
 
     toc = setup_toc(styles)
@@ -170,58 +216,41 @@ def create_pdf(book: Book) -> None:
 
     def add_toc_entry(flowable):
         if isinstance(flowable, Paragraph) and flowable.style.name in [
-            "Heading1",
-            "Heading2",
+            STYLE_HEADING1,
+            STYLE_HEADING2,
         ]:
-            level = 0 if flowable.style.name == "Heading1" else 1
+            level = 0 if flowable.style.name == STYLE_HEADING1 else 1
             text = flowable.getPlainText()
             toc.addEntry(level, text, doc.page)
 
     doc.afterFlowable = add_toc_entry
 
     for category, names in binder.items():
-        category_page = create_paragraph(category, styles["Heading1"])
+        category_page = create_paragraph(category, styles[STYLE_HEADING1])
         add_item_to_story(story, AfterSection.PAGE_BREAK, category_page)
 
         for name, content in names.items():
-            name_header = create_paragraph(name, styles["Heading2"])
+            name_header = create_paragraph(name, styles[STYLE_HEADING2])
             add_item_to_story(story, AfterSection.SPACER, name_header)
 
-            image_path: str = cast(str, content.get("image", ""))
-            if image_path and os.path.exists(image_path):
-                img = Image(image_path, width=IMAGE_WIDTH, height=IMAGE_HEIGHT)
-                add_item_to_story(story, AfterSection.SPACER, img)
+            if image_path := cast(str, content.get("image", "")):
+                add_image(story, image_path)
 
             if summary := cast(str, content.get("summary", "")):
-                summary_paragraph = create_paragraph(summary, styles["Normal"])
-                add_item_to_story(
-                    story, AfterSection.SPACER, summary_paragraph
+                summary_paragraph = create_paragraph(
+                    summary, styles[STYLE_NORMAL]
                 )
-
-            if category in {"Characters", "Settings"}:
-                for trait, chapters in content.items():
-                    if trait == "summary":
-                        continue
-                    trait_paragraph = create_paragraph(
-                        trait, styles["Heading3"]
-                    )
-                    detail_list = create_detail_list(
-                        cast(dict, chapters), styles
-                    )
-                    add_item_to_story(
-                        story,
-                        AfterSection.PAGE_BREAK,
-                        trait_paragraph,
-                        ListFlowable(detail_list),
-                    )
-
-            else:
-                detail_list = create_detail_list(cast(dict, content), styles)
                 add_item_to_story(
                     story,
-                    AfterSection.PAGE_BREAK,
-                    trait_paragraph,
-                    ListFlowable(detail_list),
+                    AfterSection.SPACER,
+                    summary_paragraph,
                 )
+
+            add_content(
+                story,
+                content,
+                styles,
+                is_traits=(category in ["Characters", "Settings"]),
+            )
 
     doc.multiBuild(story)
