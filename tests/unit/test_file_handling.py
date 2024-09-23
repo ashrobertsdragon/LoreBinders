202a275a0f7027b9b9fbe456ad05fc7634e1af09
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-09-23 15:28:20 -0600

    Fix file_handling tests broken by Path transition

--- tests/unit/test_file_handling.py
+++ tests/unit/test_file_handling.py
@@ -1,6 +1,6 @@
 import json
-import os
 import tempfile
+from pathlib import Path
 from unittest.mock import patch
 
 import pytest
@@ -15,44 +15,43 @@ from lorebinders.file_handling import (
     write_to_file,
 )
 
-
 @pytest.fixture
 def temp_file():
     with tempfile.NamedTemporaryFile(mode="w", delete=False) as tmp:
-        temp_file_path = tmp.name
+        temp_file_path = Path(tmp.name)
     yield temp_file_path
-    os.unlink(temp_file_path)
+    temp_file_path.unlink()
 
 
 def test_append_json_file_new_file(temp_file):
-    os.unlink(temp_file)
+    temp_file.unlink()
     content = [1, 2, 3]
     append_json_file(content, temp_file)
-    with open(temp_file) as f:
+    with temp_file.open() as f:
         assert json.load(f) == content
 
 
 def test_append_json_file_existing_file_list(temp_file):
     initial_content = [1, 2]
-    json.dump(initial_content, open(temp_file, "w"))
+    json.dump(initial_content, temp_file.open("w"))
     new_content = [3, 4]
     append_json_file(new_content, temp_file)
-    with open(temp_file) as f:
+    with temp_file.open() as f:
         assert json.load(f) == initial_content + new_content
 
 
 def test_append_json_file_existing_file_dict(temp_file):
     initial_content = {"a": 1, "b": 2}
-    json.dump(initial_content, open(temp_file, "w"))
+    json.dump(initial_content, temp_file.open("w"))
     new_content = {"c": 3, "d": 4}
     append_json_file(new_content, temp_file)
-    with open(temp_file) as f:
+    with temp_file.open() as f:
         assert json.load(f) == {"a": 1, "b": 2, "c": 3, "d": 4}
 
 
 def test_append_json_file_type_mismatch_error(temp_file):
     initial_content = [1, 2]
-    json.dump(initial_content, open(temp_file, "w"))
+    json.dump(initial_content, temp_file.open("w"))
     new_content = {"a": 1}
     with pytest.raises(TypeError):
         append_json_file(new_content, temp_file)
@@ -97,21 +96,21 @@ def test_separate_into_chapters_no_chapters():
 
 def test_read_text_file(temp_file):
     content = "Hello, World!"
-    with open(temp_file, "w") as f:
+    with temp_file.open("w") as f:
         f.write(content)
     assert read_text_file(temp_file) == content
 
 
 def test_read_json_file_list(temp_file):
     content = [1, 2, 3]
-    with open(temp_file, "w") as f:
+    with temp_file.open("w") as f:
         json.dump(content, f)
     assert read_json_file(temp_file) == content
 
 
 def test_read_json_file_dict(temp_file):
     content = {"a": 1, "b": 2}
-    with open(temp_file, "w") as f:
+    with temp_file.open("w") as f:
         json.dump(content, f)
     assert read_json_file(temp_file) == content
 
@@ -121,7 +120,7 @@ def test_write_to_file(temp_file):
     content2 = "This is a test."
     write_to_file(content1, temp_file)
     write_to_file(content2, temp_file)
-    with open(temp_file) as f:
+    with temp_file.open() as f:
         lines = f.readlines()
     assert lines == ["Hello, World!\n", "This is a test.\n"]
 
@@ -129,19 +128,19 @@ def test_write_to_file(temp_file):
 def test_write_json_file_list(temp_file):
     content = [1, 2, 3]
     write_json_file(content, temp_file)
-    with open(temp_file) as f:
+    with temp_file.open() as f:
         assert json.load(f) == content
 
 
 def test_write_json_file_dict(temp_file):
     content = {"a": 1, "b": 2}
     write_json_file(content, temp_file)
-    with open(temp_file) as f:
+    with temp_file.open() as f:
         assert json.load(f) == content
 
 
 def test_read_text_file_io_error(temp_file):
-    with patch("lorebinders.file_handling.pathlib.Path.read_text", side_effect=OSError):
+    with patch("lorebinders.file_handling.Path.read_text", side_effect=OSError):
         with pytest.raises(OSError):
             read_text_file(temp_file)
 
