3f99f2094cc13085f95ca7714f994b4d14636002
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2025-05-28 20:36:35 -0600

    chore(dev): convert from stubs to py.typed

--- stubs/error_handler.pyi
+++
@@ -1,9 +0,0 @@
-from _managers import EmailManager as EmailManager
-from _managers import ErrorManager
-from _types import Book as Book
-from _types import Logger as Logger
-
-class ErrorHandler(ErrorManager):
-    def __init__(cls, book: Book, email_manager: EmailManager) -> None: ...
-    @classmethod
-    def kill_app(cls, e: Exception) -> None: ...
--- stubs/make_pdf.pyi
+++
@@ -1,5 +0,0 @@
-from _typeshed import Incomplete
-
-file_handler: Incomplete
-
-def create_pdf(folder_name: str, title: str) -> None: ...
